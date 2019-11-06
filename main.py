895e83e5931b33f1b352e9bbdf683fcaf0a32cab
mozza5 <mozzarella1103@gmail.com>
2019-11-06 22:51:55 +0900

    19.11.06 22:50 진행중: 디폴트 정보로 인플루언서 추천 todo: list 형태로 아이디 리턴, 인플루언서 데이터 DB 삽입

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from flask import Flask
 from flask_restplus import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 import models
-from models import db
 import targeting
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://remoteyum:swmswm10@52.79.184.45/DNBN?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://아이디:패스워드0@52.79.184.45/DNBN?charset=utf8'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 models.db.init_app(app)
@@ -19,19 +18,23 @@ api = Api(app, version='1.0', title='Recommendation API',
 
 @api.route('/set/target/<user_id>')
 class setTargetForStore(Resource):
-    @api.doc('get')
-    def get(self, user_id):
+    @api.doc('post')
+    def post(self, user_id):
         shop = models.Shop.query.filter_by(provider_user_id =user_id).first()
         # + 로 문자열 append 하는 거 수정해야함
         addr = shop.addr_city +" " + shop.addr_dong + " " + shop.addr_dong + " " + shop.addr_detail
-        print(addr)
-        area_code = targeting.findArea(shop,addr)
-        shop.area_code = area_code
-        db.session.add(shop)
-        db.session.commit()
-
+        targeting.findArea(shop,addr)
+        models.db.session.add(shop)
+        models.db.session.commit()
         return {"text": "테스트"}
 
+@api.route('/recommend/default/<user_id>')
+class defaultRecommendationList(Resource):
+    @api.doc('get')
+    def get(self,user_id):
+        shop = models.Shop.query.filter_by(provider_user_id =user_id).first()
+        targeting.recommendation_base(shop)
+        return {"text": 'default'}
 
 
 if __name__ == "__main__":
--- targeting.py
+++ targeting.py
@@ -7,11 +7,12 @@ import models
 
 area = pd.read_csv('./data/area.csv',index_col='area_code')
 population = pd.read_csv('./data/population_rating.csv',index_col='area_code')
+influencers = pd.read_csv('./data/influencer.csv',index_col= 'channel_id')
 
 def findArea(shop,addr):
     print("도착 ",addr)
     headers = {
-        'Authorization': 'key',
+        'Authorization': 'key!',
     }
     data = {
         'query' : addr
@@ -20,7 +21,7 @@ def findArea(shop,addr):
     j = json.loads(response.text)
     WGS_y = j.get("documents")[0].get("road_address").get('y')
     WGS_x = j.get("documents")[0].get("road_address").get('x')
-
+    h_code = j.get("documents")[0].get("address").get('h_code')
     MIN = 999999
     index = -1
     for i in area.index:
@@ -30,7 +31,52 @@ def findArea(shop,addr):
         if temp < MIN:
             index = i
             MIN = temp
-    print(index)
+    shop.area_code = index
+    shop.h_code = h_code
+    shop.target_m = population.loc[index,'m']
+    shop.target_w = population.loc[index, 'fm']
+    shop.target_10 = population.loc[index,'age10']
+    shop.target_20 = population.loc[index,'age20']
+    shop.target_30 = population.loc[index,'age30']
+    shop.target_40 = population.loc[index,'age40']
+    shop.target_50 = population.loc[index,'age50']
+    shop.target_60 = population.loc[index,'age60']
+    return 'sucess'
 
-    return index;
+
+def recommendation_base(shop):
+    prediction = []
+    ageRate = population.loc[shop.area_code, ['m','fm','age10','age20','age30','age40','age50','age60']].values
+    for idx in influencers.index:
+        rating =0
+        #카테고리 비교
+        if int(shop.category_id / 100000) == int(influencers.loc[idx]['category_id'] / 100000):
+            rating = rating + 3
+        # 지역
+        if int(shop.h_code/100000000)== int(influencers.loc[idx]['addr_code'] / 100000000):
+            rating = rating + 2
+            if int(shop.h_code / 100000) == int(influencers.loc[idx]['addr_code'] / 100000):
+                rating = rating + 1
+
+            # 성별
+        gender = 1 - math.pow(population.loc[shop.area_code]['m'] - influencers.loc[idx]['target_m'],
+                              2) / 10000
+        rating = rating + gender * 2
+
+        # 연령
+        age2 = influencers.loc[
+            idx, ['target_m', 'target_w', 'target_10', 'target_20', 'target_30', 'target_40', 'target_50',
+                'target_60']].values
+        # 코사인유사도
+        age = np.dot(ageRate, age2) / (np.linalg.norm(ageRate) * np.linalg.norm(age2))
+        rating = rating + 2 * age
+        prediction.append((idx, rating))
+        # df.loc[idx] = [i, rating]
+        # idx = idx+1
+    prediction = sorted(prediction,key=lambda x:x[1], reverse=True)
+    top_10_pred = prediction[:10]
+    for i in range(10):
+        print(top_10_pred[i][0]," : ",top_10_pred[i][1])
+    return prediction
+
 
